9b1211cc443a18e0937e50529967dfbb95cb4da0
stufromnorthvan <stumitchell@irsss.ca>
2022-09-26 12:09:54 -0700

    creating test folder and exporting assertEqual and head functions into it for testing purposes

--- eqObjects.js
+++ eqObjects.js
@@ -7,16 +7,17 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arrOne, arrTwo) {
+  let compareResult = true;
   if (arrOne.length !== arrTwo.length) {
-    return false;
+    compareResult = false;
   }
   for (x = 0; x < arrOne.length; x++) {
     if (arrOne[x] !== arrTwo[x]) {
-      return false;
+      compareResult = false;
     }
   }
-  return true
-};
+  return compareResult;
+}
 
 const eqObjects = function(object1, object2) {
   let object1Keys = Object.keys(object1);
@@ -36,16 +37,16 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(assertEqual(eqObjects(cd, dc), true)); // => true
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(assertEqual(eqObjects(cd, cd2), false)); // => false
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false); // => false
 
 // const ab = { a: "1", b: "2" };
 // const ba = { b: "2", a: "1" };
-// console.log(assertEqual(eqObjects(ab, ba), true)); // => true
+// assertEqual(eqObjects(ab, ba), true); // => true
 
 // const abc = { a: "1", b: "2", c: "3" };
-// console.log(assertEqual(eqObjects(ab, abc), false)); // => false
+// assertEqual(eqObjects(ab, abc), false); // => false
---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual.js');
+const head = require('../head.js');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
